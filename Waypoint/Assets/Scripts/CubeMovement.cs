6a89b11b0fac01b246e94fae779071fc50756733
unknown <15191901@W1801208.portoalegre.pucrsnet.br>
2015-10-15 20:43:05 -0300

    Waypoint project

---
+++ Waypoint/Assets/Scripts/CubeMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeMovement : MonoBehaviour {
+
+    void wake ()
+    {
+
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
---
+++ Waypoint/Assets/Scripts/Waypoint_test.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoint_test : MonoBehaviour
+{
+
+    public GameObject Cube;
+    public GameObject way1;
+    public GameObject way2;
+    public GameObject way3;
+    public GameObject way4;
+
+ 	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
