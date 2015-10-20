bf8aa30f3d739b29470c1ac9274f04b70000ac0b
hellwilliam <gbourscheidt@hotmail.com>
2015-10-20 18:03:27 -0200

    Movimento simples barril

---
+++ Waypoint/Assets/Scripts/BarrelMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarrelMovement : MonoBehaviour
+{
+    public int speed = 100;
+    Vector3 direction = Vector3.forward;
+    Vector3 movePosition = Vector3.zero;
+    Vector3 newPosition = Vector3.zero;
+
+
+
+    public void SearchForOtherWaypoint(Waypoint waypoint)
+    {
+        //waypoint.next
+    }
+    void wake()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = direction * (speed * Time.deltaTime);
+        newPosition = transform.position + newPosition;
+    }
+}
