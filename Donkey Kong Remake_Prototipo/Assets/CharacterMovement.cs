e36d4372f8bcba0bfcfb1e447bc87659960abd40
hellwilliam <gbourscheidt@hotmail.com>
2015-10-27 18:19:23 -0200

    Jump test

--- Donkey Kong Remake_Prototipo/Assets/CharacterMovement.cs
+++ Donkey Kong Remake_Prototipo/Assets/CharacterMovement.cs
@@ -5,6 +5,8 @@ public class CharacterMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Transform cameraPivot;
+    public int jumpForce = 5;
+
        
     // Use this for initialization
 	void Start () {
@@ -23,7 +25,25 @@ public class CharacterMovement : MonoBehaviour
         rigidbody.MovePosition(rigidbody.position + moveDirection);
 
         cameraPivot.position = rigidbody.position;
+
+        jumpTest();
 	}
 
+    void jumpTest ()
+    {
+        //Criar a lógica básica para o pulo
+        //if (Input.GetButtonDown("space"))
+        //{
+        //    transform.Translate(Vector3.up * jumpForce * Time.deltaTime, Space.World);
+        //}
+
+        if (Input.GetKeyDown (KeyCode.Space) || Input.GetButtonDown("Jump"))
+        {
+            rigidbody.velocity += jumpForce * Vector3.up;
+        }
+
+
+
+    }
 
 }
