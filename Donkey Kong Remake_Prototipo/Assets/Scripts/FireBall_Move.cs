e8f513c838c64b803c64672dd2c349a2b4cbadf0
unknown <15191901@W1801208.portoalegre.pucrsnet.br>
2015-11-12 22:20:03 -0200

    Bolinha de fogo disparando adoidado

--- Donkey Kong Remake_Prototipo/Assets/Scripts/FireBall_Move.cs
+++ Donkey Kong Remake_Prototipo/Assets/Scripts/FireBall_Move.cs
@@ -1,15 +1,73 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FireBall_Move : MonoBehaviour {
+public class FireBall_Move : MonoBehaviour 
+{
+    public Transform stuff;
+    public Vector3 vel;
+    public float swithDirectrion = 3f;
+    public float curTime = 0;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+    {
+        SetVel();	
 	}
+
+    private void SetVel()
+    {
+        if (Random.value > .5)
+        {
+            vel.x = 4 * 4 * Random.value;
+        }
+        else
+        {
+            vel.x = -4 * 4 * Random.value;
+        }
+
+        if (Random.value > .5)
+        {
+            vel.z = 4 * 4 * Random.value;
+        }
+
+        else
+        {
+            vel.z = -4 * 4 * Random.value;
+        }
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+        if (curTime < swithDirectrion)
+        {
+            curTime += 1 * Time.deltaTime;
+        }
+
+        else
+        {
+            SetVel();
+            if (Random.value > 5)
+            {
+                swithDirectrion += Random.value;
+            }
+
+            else
+            {
+                swithDirectrion -= Random.value;
+            }
+        }
+
+        if (swithDirectrion < 1)
+        {
+            swithDirectrion = 1 + Random.value;
+        }
+
+        curTime = 0;
+  	}
+
+    void FixedUpdate()
+    {
+        stuff.rigidbody.velocity = vel;
+    }
 }
