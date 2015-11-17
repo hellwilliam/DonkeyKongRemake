864f07e6a9a9289b833d877600bf2e6a1f466867
unknown <15191901@W1801208.portoalegre.pucrsnet.br>
2015-11-17 21:19:25 -0200

    Tentativa de movimentacao da FireBall

--- Donkey Kong Remake_Prototipo/Assets/Scripts/FireBall_Move.cs
+++ Donkey Kong Remake_Prototipo/Assets/Scripts/FireBall_Move.cs
@@ -3,71 +3,87 @@ using System.Collections;
 
 public class FireBall_Move : MonoBehaviour 
 {
-    public Transform stuff;
-    public Vector3 vel;
-    public float swithDirectrion = 3f;
-    public float curTime = 0;
+    //My Code-------------------------------------------------------------------------
+    public float stepSpeed = 2f;
+    public Transform moveFireBall;
+    public Vector3 moveFoward;
 
-	// Use this for initialization
-	void Start () 
+    void Speed()
     {
-        SetVel();	
-	}
-
-    private void SetVel()
-    {
-        if (Random.value > .5)
-        {
-            vel.x = 4 * 4 * Random.value;
-        }
-        else
-        {
-            vel.x = -4 * 4 * Random.value;
-        }
-
-        if (Random.value > .5)
-        {
-            vel.z = 4 * 4 * Random.value;
-        }
-
-        else
-        {
-            vel.z = -4 * 4 * Random.value;
-        }
+        moveFoward.z = 1 * Time.deltaTime;
     }
+    
+    void Update ()
+    {
+        moveFireBall.rigidbody.velocity = moveFoward;
+    }
+    //--------------------------------------------------------------------------------
+
+    //public Transform stuff;
+    //public Vector3 vel;
+    //public float swithDirectrion = 3f;
+    //public float curTime = 0;
+
+    //// Use this for initialization
+    //void Start () 
+    //{
+    //    SetVel();	
+    //}
+
+    //private void SetVel()
+    //{
+    //    if (Random.value > .5)
+    //    {
+    //        vel.x = 4 * 4 * Random.value;
+    //    }
+    //    else
+    //    {
+    //        vel.x = -4 * 4 * Random.value;
+    //    }
+
+    //    if (Random.value > .5)
+    //    {
+    //        vel.z = 4 * 4 * Random.value;
+    //    }
+
+    //    else
+    //    {
+    //        vel.z = -4 * 4 * Random.value;
+    //    }
+    //}
 	
-	// Update is called once per frame
-	void Update ()
-    {
-        if (curTime < swithDirectrion)
-        {
-            curTime += 1 * Time.deltaTime;
-        }
+    //// Update is called once per frame
+    //void Update ()
+    //{
+    //    if (curTime < swithDirectrion)
+    //    {
+    //        curTime += 1 * Time.deltaTime;
+    //    }
 
-        else
-        {
-            SetVel();
-            if (Random.value > 5)
-            {
-                swithDirectrion += Random.value;
-            }
+    //    else
+    //    {
+    //        SetVel();
+    //        if (Random.value > 5)
+    //        {
+    //            swithDirectrion += Random.value;
+    //        }
 
-            else
-            {
-                swithDirectrion -= Random.value;
-            }
-        }
+    //        else
+    //        {
+    //            swithDirectrion -= Random.value;
+    //        }
+    //    }
 
-        if (swithDirectrion < 1)
-        {
-            swithDirectrion = 1 + Random.value;
-        }
+    //    if (swithDirectrion < 1)
+    //    {
+    //        swithDirectrion = 1 + Random.value;
+    //    }
 
-        curTime = 0;
-  	}
+    //    curTime = 0;
+    //}
 
-    void FixedUpdate()
-    {
-        stuff.rigidbody.velocity = vel;
-    }
+    //void FixedUpdate()
+    //{
+    //    stuff.rigidbody.velocity = vel;
+    //}
 }
