be20f61a33894bc8bd2999198ea800d7774c2742
hellwilliam <gbourscheidt@hotmail.com>
2015-12-03 17:35:37 -0200

    Testing camera rotation

--- Donkey Kong Remake_Prototipo/Assets/Scripts/CameraInput.cs
+++ Donkey Kong Remake_Prototipo/Assets/Scripts/CameraInput.cs
@@ -9,14 +9,38 @@ public class CameraInput : MonoBehaviour {
 
     public float ySpeed = 90;
     public float zSpeed = 45;
+
+    public float speed = 55;
+    private float rotation = 0;
+    private Quaternion qTo = Quaternion.identity;
+      
+
+    //public Input m_prevGetAxis;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
+       
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Rotate();
         FollowPlayer();
+
+        //if (Input.GetKeyDown(KeyCode.Q))
+        //{
+        //    rotation += 90;
+        //    qTo = Quaternion.Euler(0, 0, rotation);
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.E))
+        //{
+        //    rotation = -90;
+        //    qTo = Quaternion.Euler(0, 0, rotation);
+        //}
+
+        //transform.rotation = Quaternion.RotateTowards(transform.rotation, qTo, speed * Time.deltaTime);
 	}
 
     private void FollowPlayer()
@@ -32,6 +56,12 @@ public class CameraInput : MonoBehaviour {
         float h = Input.GetAxis("CameraRotation") * Time.deltaTime;
         Vector3 rotation = cameraPivot.eulerAngles;
         rotation.y = rotation.y + h * ySpeed;
+
         cameraPivot.eulerAngles = rotation;
     }
+
+    //private void UpDate()
+    //{
+    //    m_prevGetAxis = Input.GetAxis("CameraRotation");
+    //}
 }
