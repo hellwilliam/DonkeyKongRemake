b0388eaeb3bc2c0f789558e5d32df0d610cbfde7
unknown <15191901@W1801208.portoalegre.pucrsnet.br>
2015-12-04 18:40:37 -0200

    Main menu added

---
+++ Donkey Kong Remake_Prototipo/Assets/Scripts/NewScene.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewScene : MonoBehaviour
+{    
+    public void LoadScene (string level)
+    {
+         Application.LoadLevel(level);
+    }
+
+	// Use this for initialization
+    //void Start () 
+    //{
+	
+    //}
+	
+    //Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+}
---
+++ Donkey Kong Remake_Prototipo/Assets/Scripts/QuitGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour 
+{
+    public void Quit()
+    {
+        Debug.Log("I quit");
+        Application.Quit();
+    }
+}
